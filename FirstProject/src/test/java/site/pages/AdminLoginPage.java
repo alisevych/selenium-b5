9a0790160a83bee5e4805eb1b62f14f23dd2e5dd
Alena Lisevych <alena.lisevych@firstlinesoftware.com>
2019-05-24 17:17:32 +0300

    created loginAsAdmin_All_in_one_place_Test(); changed names of locators;

--- FirstProject/src/test/java/site/pages/AdminLoginPage.java
+++ FirstProject/src/test/java/site/pages/AdminLoginPage.java
@@ -4,8 +4,9 @@ import org.openqa.selenium.By;
 
 public class AdminLoginPage extends BasePage {
 
-    public static final String USERNAME_NAME = "username";
-    public static final String PASSWORD_NAME = "password";
+    public static final String USERNAME_INPUT_NAME = "username";
+    public static final String PASSWORD_INPUT_NAME = "password";
+    public static final String LOGIN_BUTTON_NAME = "login";
 
     public AdminLoginPage(){
         url = "http://localhost/litecart/admin/";
@@ -13,7 +14,8 @@ public class AdminLoginPage extends BasePage {
     }
 
     public void loginAsAdmin(String username, String password){
-        driver.findElement(By.name(USERNAME_NAME)).sendKeys(username);
-        driver.findElement(By.name(PASSWORD_NAME)).sendKeys(password);
+        driver.findElement(By.name(USERNAME_INPUT_NAME)).sendKeys(username);
+        driver.findElement(By.name(PASSWORD_INPUT_NAME)).sendKeys(password);
+        driver.findElement(By.name(LOGIN_BUTTON_NAME)).click();
     }
 }
