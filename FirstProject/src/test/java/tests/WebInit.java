ff16912a97d52f1679f1487ea2ac6e733ab697d4
Alena Lisevych <alena.lisevych@firstlinesoftware.com>
2019-05-24 14:59:49 +0300

    implicitlyWait added to WebInit

--- FirstProject/src/test/java/tests/WebInit.java
+++ FirstProject/src/test/java/tests/WebInit.java
@@ -6,15 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class WebInit {
 
     public WebDriver driver;
     public WebDriverWait driverWait;
-    public static final int timeout = 20;
+    public static final int timeout = 10;
 
     @Before
     public void start() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
         driverWait = new WebDriverWait(driver, timeout);
     }
 
