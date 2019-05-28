eee35c8beb87a8ab669540e11a78a57ca40dafbf
Alena Lisevych <alena.lisevych@firstlinesoftware.com>
2019-05-28 19:46:58 +0300

    Firefox default, Firefox ESR 45, Firefox Nightly, IExplorer can be started

--- LitecartLisevych/src/test/java/tests/WebInit.java
+++ LitecartLisevych/src/test/java/tests/WebInit.java
@@ -3,12 +3,19 @@ package tests;
 import helpers.InputHelper;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class WebInit {
@@ -20,17 +27,38 @@ public class WebInit {
     public static final String CHROME_NAME = "chrome";
     public static final String IE_NAME = "IExplorer";
     public static final String FIREFOX_NAME = "firefox";
+    public static final String FIREFOX_OLD_NAME = "firefoxOld";
+    public static final String FIREFOX_NIGHTLY_NAME = "firefoxNightly";
 
     @Before
     public void start() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("unexpectedAlertBehaviour", "dismiss");
         String browserName = new InputHelper().getPropertyValue("driver");
-        if (browserName.equals(CHROME_NAME))
-            driver = new ChromeDriver();
-        if (browserName.equals(IE_NAME))
-            driver = new InternetExplorerDriver();
-        if (browserName.equals(FIREFOX_NAME))
+        if (browserName.equals(CHROME_NAME)) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("start-fullscreen");
+            capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+            driver = new ChromeDriver(capabilities);
+        }
+        if (browserName.equals(IE_NAME)) {
+            driver = new InternetExplorerDriver(capabilities);
+        }
+        if (browserName.equals(FIREFOX_NAME)) {
             driver = new FirefoxDriver();
-
+        }
+        if (browserName.equals(FIREFOX_OLD_NAME)) {
+            FirefoxOptions ffOptions = new FirefoxOptions();
+            ffOptions.setBinary(new FirefoxBinary(new File("c:\\Program Files\\Mozilla Firefox\\45.ESR\\firefox.exe")));
+            capabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, ffOptions);
+            driver = new FirefoxDriver(capabilities);
+        }
+        if (browserName.equals(FIREFOX_NIGHTLY_NAME)) {
+            FirefoxOptions ffOptions = new FirefoxOptions();
+            ffOptions.setBinary(new FirefoxBinary(new File("c:\\Program Files\\Mozilla Firefox\\Nightly\\firefox.exe")));
+            driver = new FirefoxDriver(ffOptions);
+        }
+        System.out.println("[AL] Capabilities::\n" + ((HasCapabilities) driver).getCapabilities());
         driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
         driverWait = new WebDriverWait(driver, timeout);
     }
